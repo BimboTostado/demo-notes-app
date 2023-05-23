762de6af482b33f4276a5c4af532adb8f1ff324c
agarcia <agarcia@ivolutia.com>
2023-05-23 11:51:47 -0300

    actualizaciones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,6 @@ function App() {
       userHasAuthenticated(true);
     } catch (e) {
       if (e !== "No current user") {
-        //alert(e);
         onError(e);
       }
     }
@@ -44,31 +43,36 @@ function App() {
   return (
     !isAuthenticating && (
       <div className="App container py-3">
-          <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-            <LinkContainer to="/">
-              <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Nav activeKey={window.location.pathname}>
-                {isAuthenticated ? (
+        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <>
+                  <LinkContainer to="/settings">
+                    <Nav.Link>Settings</Nav.Link>
+                  </LinkContainer>
                   <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                ) : (
-                  <>
-                    <LinkContainer to="/signup">
-                      <Nav.Link>Signup</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/login">
-                      <Nav.Link>Login</Nav.Link>
-                    </LinkContainer>
-                  </>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-            <Routes />
-          </AppContext.Provider>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
       </div>
     )
   );
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,4 @@
 const config = {
-  // Backend config
-  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: process.env.REACT_APP_REGION,
     BUCKET: process.env.REACT_APP_BUCKET,
@@ -15,6 +13,7 @@ const config = {
     APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
     IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
   },
+  MAX_ATTACHMENT_SIZE: 5000000,
 };
 
 export default config;
